fac5c8fd1f5e757aeb36237441a56ff2aaccec41
Syed Ehtesham <ehtesham0337@gmail.com>
2022-09-14 19:15:01 +0500

    fetch from db.json using axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Persons from './Components/Persons.js'
 import NewPersonForm from './Components/NewPersonForm.js'
 import SearchFilter from './Components/SearchFilter.js'
 
 const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-    {name : 'Nathan Drake' , number: '92-1111111', id: 5}
-])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -33,6 +28,12 @@ const App = (props) => {
 
   //   return true
   // }
+
+  useEffect(() => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => setPersons(response.data))
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
